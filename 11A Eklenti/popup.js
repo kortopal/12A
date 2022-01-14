03793f84fcc6801e3a10b84af605a69ed17435a8
kortopal <95717415+kortopal@users.noreply.github.com>
2022-01-14 18:36:06 +0300

    Update popup.js

--- 11A Eklenti/popup.js
+++ 11A Eklenti/popup.js
@@ -1,3 +1,5 @@
+var input1 = document.getElementById("input1");
+
 function go(url) {
     window.open(url, "_blank");
 }
@@ -22,9 +24,61 @@ document.getElementById("btn5").addEventListener("click", function() {
     go("http://timer.onlinealarmkur.com/" + "#" + document.getElementById("input1").value);
 });
 
-document.getElementById("input1").addEventListener("keyup", function(event) {
+input1.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById("btn5").click();
     }
+});
+
+input1.addEventListener("click", function() {
+    document.getElementById("numpad").style.display = "block";
+    document.getElementById("numpad").scrollIntoView();
+});
+
+var newinputvalue;
+function numpadAdd(num) {
+    newinputvalue = parseInt(document.getElementById("input1").value + num);
+    input1.value = newinputvalue;
+}
+
+document.getElementById("num1").addEventListener("click", function() {
+    numpadAdd(1);
+});
+document.getElementById("num2").addEventListener("click", function() {
+    numpadAdd(2);
+});
+document.getElementById("num3").addEventListener("click", function() {
+    numpadAdd(3);
+});
+document.getElementById("num4").addEventListener("click", function() {
+    numpadAdd(4);
+});
+document.getElementById("num5").addEventListener("click", function() {
+    numpadAdd(5);
+});
+document.getElementById("num6").addEventListener("click", function() {
+    numpadAdd(6);
+});
+document.getElementById("num7").addEventListener("click", function() {
+    numpadAdd(7);
+});
+document.getElementById("num8").addEventListener("click", function() {
+    numpadAdd(8);
+});
+document.getElementById("num9").addEventListener("click", function() {
+    numpadAdd(9);
+});
+document.getElementById("num0").addEventListener("click", function() {
+    numpadAdd(0);
+});
+document.getElementById("numback").addEventListener("click", function() {
+    input1.value = parseInt(input1.value.toString().slice(0, -1));
+});
+document.getElementById("numclose").addEventListener("click", function() {
+    input1.value = null;
+    document.getElementById("numpad").style.display = "none";
+});
+document.getElementById("numstart").addEventListener("click", function() {
+    document.getElementById("btn5").click();
 });
