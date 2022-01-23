76a38ecf1d615e93462a75c4b6550b8d2a354d7f
kortopal <95717415+kortopal@users.noreply.github.com>
2022-01-23 13:19:17 +0300

    Add files via upload

---
+++ 11A Eklenti/js/functions.js
@@ -0,0 +1,17 @@
+function getDateAndTime(command) {
+    var date = new Date();
+    switch(command){
+        case "Day":
+            return (date.getDate()<10? "0" : "") + date.getDate();
+        case "Month":
+            return months_tr[date.getMonth()];
+        case "Year":
+            return date.getFullYear();
+        case "Hours":
+            return (date.getHours()<10? "0" : "") + date.getHours();
+        case "Minutes":
+            return (date.getMinutes()<10? "0" : "") + date.getMinutes();
+        case "DateAndTime":
+            return (date.getDate()<10? "0" : "") + date.getDate() + " " + months_tr[date.getMonth()] + " " + date.getFullYear() + " - " + (date.getHours()<10? "0" : "") + date.getHours() + ":" + (date.getMinutes()<10? "0" : "") + date.getMinutes();
+    }
+}
